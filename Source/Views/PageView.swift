b32e3d53b00d6cb768ea041714a8c8656e85521e
Vadim Balashov <balashov@corp.mail.ru>
2019-03-04 16:06:41 +0300

    Fixed bug when zoomed image become half screen sized after rotation

--- Source/Views/PageView.swift
+++ Source/Views/PageView.swift
@@ -207,10 +207,10 @@ class PageView: UIScrollView {
 extension PageView: LayoutConfigurable {
 
   @objc func configureLayout() {
+    zoomScale = minimumZoomScale
     contentFrame = frame
     contentSize = frame.size
     imageView.frame = frame
-    zoomScale = minimumZoomScale
 
     configureImageView()
   }
